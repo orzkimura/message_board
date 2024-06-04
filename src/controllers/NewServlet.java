2a40c66de2fbaf47d4f1064fb03a938a2e8d5241
yuuma.kimura2 <y_kimura@horizon-so.jp>
2024-06-04 10:03:09 +0900

    Index/NewServletの修正

--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -3,6 +3,7 @@ package controllers;
 import java.io.IOException;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -34,31 +35,15 @@ public class NewServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	    EntityManager em = DBUtil.createEntityManager();
-	    em.getTransaction().begin();
+	   // CSRF対策
+	    request.setAttribute("_token", request.getSession().getId());
 	    
-	    // Messageのインスタンスを生成
-	    Message m = new Message();
+	    // おまじないとしてのインスタンスを生成
+	    request.setAttribute("message", new Message());
 	    
-	    // mの各フィールドにデータを代入
-	    String title = "taro";
-	    m.setTitle(title);
+	    RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/new.jsp");
+	    rd.forward(request, response);
 	    
-	    String content = "hello";
-	    m.setContent(content);
-	    
-	    Timestamp currentTime = new Timestamp(System.currentTimeMillis()); // 正しいjava.sql.Timestampを使用
-	    m.setCreated_at(currentTime);
-	    m.setUpdated_at(currentTime);
-	    
-	    // データベースに保存
-	    em.persist(m);
-	    em.getTransaction().commit();
-	    
-	    // 自動採番されたIDの値を表示
-	    response.getWriter().append(Integer.valueOf(m.getId()).toString());
-	    
-	    em.close();
 	}
 
 }
